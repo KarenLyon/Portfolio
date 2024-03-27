e8a2ce41e0dc4df75e099232cff3dd0335a0066e
Karen <karensofialyon@gmail>
2024-03-27 16:16:58 +0000

    portfolio

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Contact = () => {
+   
+
+   
+
+    return (
+        <section className="contact" id="connect">
+            <Container>
+              
+            </Container>
+        </section>
+    )
+}
